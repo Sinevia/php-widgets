66d8e22afd8fde2987fd2a1b1ba69cb4166c5eb7
Sinevia <info@sinevia.com>
2016-09-19 06:45:09 +0100

    Update Cycle2.php

--- src/Cycle2.php
+++ src/Cycle2.php
@@ -1,10 +1,20 @@
 <?php
 
+namespace Sinevia\Widgets
+
 class Cycle2 {
+  public $data = null;
   function __construct($data=[]){
-  
+    $this->data = $data;
   }
   function display(){
     $html = "";
+    $html .= '<div class="cycle-slideshow">';
+    $html .= '<img src="http://malsup.github.io/images/p1.jpg">';
+    $html .= '<img src="http://malsup.github.io/images/p2.jpg">';
+    $html .= '<img src="http://malsup.github.io/images/p3.jpg">';
+    $html .= '<img src="http://malsup.github.io/images/p4.jpg">';
+    $html .= '</div>';
+    return $html;
   }
 }
