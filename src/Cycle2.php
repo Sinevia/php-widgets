b783caaf14818fd9630f688005c0a2d28b09c3ab
Sinevia <info@sinevia.com>
2016-09-19 06:48:09 +0100

    Update Cycle2.php

--- src/Cycle2.php
+++ src/Cycle2.php
@@ -9,11 +9,13 @@ class Cycle2 {
   }
   function display(){
     $html = "";
+    $html .= '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>';
+    $html .= '<script src="http://malsup.github.com/jquery.cycle2.js"></script>';
     $html .= '<div class="cycle-slideshow">';
-    $html .= '<img src="http://malsup.github.io/images/p1.jpg">';
-    $html .= '<img src="http://malsup.github.io/images/p2.jpg">';
-    $html .= '<img src="http://malsup.github.io/images/p3.jpg">';
-    $html .= '<img src="http://malsup.github.io/images/p4.jpg">';
+    $html .= '  <img src="http://malsup.github.io/images/p1.jpg">';
+    $html .= '  <img src="http://malsup.github.io/images/p2.jpg">';
+    $html .= '  <img src="http://malsup.github.io/images/p3.jpg">';
+    $html .= '  <img src="http://malsup.github.io/images/p4.jpg">';
     $html .= '</div>';
     return $html;
   }
